8c4ed7d133085fec577b1a8517647186e7f9097e
ar-volos <136924495+ar-volos@users.noreply.github.com>
2023-08-15 14:21:12 +0000

    first steps

---
+++ calculator.py
@@ -0,0 +1,19 @@
+#! /home/codespace/.python/current/bin/python3
+print("Welcome to the Calculator Program! \n Instructions:\n 1. Prepare two desired numbers for calculation and be ready to input \n 2. Choose desired calculation action \n 3. Enjoy wih resuls")
+
+number1 = input("Please enter the first number:")
+print("Your first number is:" + number1)
+
+number2 = input("Please enter the second number:")
+print("Your second number is:" + number2)
+
+print("Please select an operation:")
+print("1. Addition")
+print("2. Subtraction")
+print("3. Multiplication")
+print("4. Division")
+
+choice = input("Enter your choice (1-4):")
+print("Enter your choice (1-4):" + choice) 
+
+
