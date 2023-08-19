bda7c97c1e78c9b934b85a474c366b7130b8d4b8
ar-volos <136924495+ar-volos@users.noreply.github.com>
2023-08-19 23:42:29 +0000

    ready to check

---
+++ README.md
@@ -0,0 +1,39 @@
+**Here you will find basic calculator program created in Python.**
+
+The program allows the user to perform simple arithmetic operations on two numbers.
+
+**Fulfilled requirements**:
+
+
+1) Prompt the user to enter two numbers.
+2) Prompt the user to select an operation from the following options: 
+- addition
+- subtraction
+- multiplication
+- division.
+3) Based on the selected operation, perform the corresponding calculation.
+4) Display the result to the user.
+
+
+```bash
+Welcome to the Calculator Program!
+Instructions:
+1. Prepare two desired numbers for calculation and be ready to input
+2. Choose the desired calculation action
+3. Enjoy with the results
+
+Please enter the first number:10
+Your first number is: 10
+Please enter the second number:20
+Your second number is: 20
+Enter your choice (1-4):1
+Your choice is: 1. Addition
+
+The result of the operation is: 30
+```
+
+
+Program handles:
+ - division by zero and provides an appropriate error message if the user attempts to divide by zero;
+ - clear instructions and confirmation for required inputs;
+ - error handling for invalid input value and invalid input type until appropriate input will be entered;
--- calculator.py
+++ calculator.py
@@ -1,19 +1,80 @@
 #! /home/codespace/.python/current/bin/python3
-print("Welcome to the Calculator Program! \n Instructions:\n 1. Prepare two desired numbers for calculation and be ready to input \n 2. Choose desired calculation action \n 3. Enjoy wih resuls")
+instructions = """
+Welcome to the Calculator Program!
+Instructions:
+1. Prepare two desired numbers for calculation and be ready to input
+2. Choose the desired calculation action
+3. Enjoy with the results
+"""
 
-number1 = input("Please enter the first number:")
-print("Your first number is:" + number1)
+print(instructions)
 
-number2 = input("Please enter the second number:")
-print("Your second number is:" + number2)
+while True:
+    try:
+        number1 = float(input("Please enter the first number:"))
+        print("Your first number is: {0:g}".format(number1))
+    except ValueError:
+        print("Error: Invalid input type. Please enter numeric values.")
+        continue
+    break
 
-print("Please select an operation:")
-print("1. Addition")
-print("2. Subtraction")
-print("3. Multiplication")
-print("4. Division")
+while True:
+    try:
+        number2 = float(input("Please enter the second number:"))
+        print("Your second number is: {0:g}".format(number2))
 
-choice = input("Enter your choice (1-4):")
-print("Enter your choice (1-4):" + choice) 
+    except ValueError:
+        print("Error: Invalid input type. Please enter numeric values.")
+        continue
+    break
 
 
+select = """
+Please select an operation:
+1. Addition
+2. Subtraction
+3. Multiplication
+4. Division
+"""
+
+choices = {
+    1: "Addition",
+    2: "Subtraction",
+    3: "Multiplication",   
+    4: "Division"
+}
+
+
+while True:
+    try:
+        choice = int(input("Enter your choice (1-4):"))
+        if choice < 1 or choice > 4:
+            raise ValueError("Wrong input value")
+        print("Your choice is: {}. {}". format(choice, choices[choice]))
+        break # Break out of the loop if the input is valid
+
+    except ValueError as ve:
+        if str(ve) == "Wrong input value":
+            print("Error: Wrong input value. Please enter a number from 1 to 4.")
+        else:
+            print("Error: Invalid input type. Please enter numeric values.")
+        continue
+
+
+  
+
+if choice == 1:
+    result = number1 + number2
+elif choice == 2:
+    result = number1 - number2
+elif choice == 3:
+    result = number1 * number2
+elif choice == 4:
+    if number2 != 0:  # Handle division by zero
+        result = number1 / number2
+    else:
+        print("Error: Division by zero is not allowed.")
+        exit(1)
+
+
+print("\nThe result of the operation is: {0:g}".format(result))
